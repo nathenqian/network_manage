bbeda95ec2b9918d7cd07fa2ad8d49db1e0875c7
Xinrui Zhu <452075061@qq.com>
2016-12-22 17:08:37 +0800

    finishRandom_2

--- randomSize.py
+++ randomSize.py
@@ -4,11 +4,15 @@ def getSize(x):
     if x < 64:
         return x/3
     if x < 128:
-        return x/6
+        return x/5
     if x < 256:
-        return x/12
+        return x/9
     if x < 512:
-        return x/24
+        return x/15
     if x < 1024:
-        return x/48
-    return x/96
+        return x/23
+    if x < 2048:
+        return x/33
+    if x < 4096:
+        return x/45
+    return x/60
